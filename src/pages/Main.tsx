eeef98c1284d7363c79a18001bf170c9536c0e8a
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-06-11 11:52:10 +0100

    Update countries loader

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,92 @@
 import { useLoaderData } from "react-router-dom";
 
+interface Country {
+  name: {
+    common: string;
+    official: string;
+    nativeName: {
+      [key: string]: {
+        official: string;
+        common: string;
+      };
+    };
+  };
+  tld: string[];
+  cca2: string;
+  ccn3: string;
+  cca3: string;
+  cioc: string;
+  independent: boolean;
+  status: string;
+  unMember: boolean;
+  currencies: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+  idd: {
+    root: string;
+    suffixes: string[];
+  };
+  capital: string[];
+  altSpellings: string[];
+  region: string;
+  subregion: string;
+  languages: {
+    [key: string]: string;
+  };
+  translations: {
+    [key: string]: {
+      official: string;
+      common: string;
+    };
+  };
+  latlng: [number, number];
+  landlocked: boolean;
+  borders: string[];
+  area: number;
+  demonyms: {
+    eng: {
+      f: string;
+      m: string;
+    };
+    fra: {
+      f: string;
+      m: string;
+    };
+  };
+  flag: string;
+  maps: {
+    googleMaps: string;
+    openStreetMaps: string;
+  };
+  population: number;
+  gini: {
+    [year: string]: number;
+  };
+  fifa: string;
+  car: {
+    signs: string[];
+    side: string;
+  };
+  timezones: string[];
+  continents: string[];
+  flags: {
+    png: string;
+    svg: string;
+    alt: string;
+  };
+  coatOfArms: {
+    png: string;
+    svg: string;
+  };
+  startOfWeek: string;
+  capitalInfo: {
+    latlng: [number, number];
+  };
+}
+
 export async function loader() {
   try {
     const response = await fetch("https://restcountries.com/v3.1/all");
@@ -10,7 +97,15 @@ export async function loader() {
 
     const countries = await response.json();
 
-    return countries;
+    const mappedCountries = countries.map((country: Country) => ({
+      flags: country.flags,
+      namse: country.name,
+      population: country.population,
+      region: country.region,
+      capital: country.capital,
+    }));
+
+    return mappedCountries;
   } catch (error) {
     throw new Error("Failed to fetch countries");
   }
@@ -18,8 +113,6 @@ export async function loader() {
 
 export default function MainPage() {
   const countries = useLoaderData();
-
-  console.log(countries);
 
   return (
     <main>
