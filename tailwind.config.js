653575b030cc014601ffacbea5be4c9efc666f1a
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-06-11 11:19:44 +0100

    Create some basic styling classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-dark": "hsl(209, 23%, 22%)",
+        "blue-very-dark": "hsl(207, 26%, 17%)",
+        "blue-very-dark-lm": "hsl(200, 15%, 8%)",
+        "gray-dark": "hsl(0, 0%, 52%)",
+        "gray-very-light-lm": "hsl(0, 0%, 98%)",
+        white: " hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
