0aa9e91236fc43401928ff016e6cd5f6b2cff20b
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-06-12 14:36:15 +0100

    Wrap App in Providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import QueryProvider from "./components/QueryProvider";
+import { Provider } from "react-redux";
 import Router from "./components/Router";
+import store from "./store/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryProvider>
-      <Router />
-    </QueryProvider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    </QueryClientProvider>
   );
 }
 
