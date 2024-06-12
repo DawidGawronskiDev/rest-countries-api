165eefa2934701d255ced77bdba7b9bf0ba77d60
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-06-12 14:46:38 +0100

    Add comments to colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,17 @@ export default {
   theme: {
     extend: {
       colors: {
+        // Dark Mode Elements
         "blue-dark": "hsl(209, 23%, 22%)",
+        // Dark Mode Background
         "blue-very-dark": "hsl(207, 26%, 17%)",
+        // Light Mode Text
         "blue-very-dark-lm": "hsl(200, 15%, 8%)",
+        // Light Mode Input
         "gray-dark": "hsl(0, 0%, 52%)",
+        // Light Mode Background
         "gray-very-light-lm": "hsl(0, 0%, 98%)",
+        // Dark Mode Text & Light Mode Elements
         white: " hsl(0, 0%, 100%)",
       },
     },
