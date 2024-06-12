07edb60a672214ff6ebc55fe381297ebad54b2cf
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-06-12 14:43:33 +0100

    Add dark className to RootLayout

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
+import { RootState } from "../store/store";
 
 export default function RootLayout() {
+  const { mode } = useSelector((state: RootState) => state.theme);
+
   return (
-    <>
+    <div className={mode === "dark" ? "dark" : undefined}>
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 }
