3c3ec5e9ab7b87fdbb0c4a16340d855ee8743fb4
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-06-12 14:41:04 +0100

    Update ThemeToggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,8 +1,16 @@
+import { useDispatch } from "react-redux";
 import MoonIcon from "./icons/MoonIcon";
+import { toggle } from "../store/themeSlice";
 
 export default function ThemeToggle() {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(toggle());
+  };
+
   return (
-    <button className="flex gap-2">
+    <button onClick={handleClick} className="flex gap-2">
       <MoonIcon />
       <p className="text-base font-semibold">Dark Mode</p>
     </button>
