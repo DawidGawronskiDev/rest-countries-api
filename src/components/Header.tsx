ccbd30802e76e6c0361a34c4c56e4a58c316d49d
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-06-12 14:18:19 +0100

    Update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import Wrapper from "./Wrapper";
+
 export default function Header() {
   return (
-    <header>
-      <h1 className="text-2xl font-extrabold">Header!</h1>
+    <header className="h-20 flex items-center justify-center">
+      <Wrapper>
+        <h1 className="text-2xl font-extrabold">Where in the world?</h1>
+      </Wrapper>
     </header>
   );
 }
